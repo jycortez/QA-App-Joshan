12a97f00048fe93f8daec0455e5ff6e73b7f53da
jycortez <jycortez@email.uark.edu>
2017-10-19 08:42:30 -0500

    Welcome Form Name

--- QuestionsAnswersApp/Form1.vb
+++ QuestionsAnswersApp/Form1.vb
@@ -1,3 +1,5 @@
-﻿Public Class Form1
+﻿Public Class Welcome
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
 End Class
--- QuestionsAnswersApp/My Project/Application.Designer.vb
+++ QuestionsAnswersApp/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.QuestionsAnswersApp.Form1
+            Me.MainForm = Global.QuestionsAnswersApp.Welcome
         End Sub
     End Class
 End Namespace
